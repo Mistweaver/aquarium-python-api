4f763888e5084f775cfec6bbb58ef1557c278387
Mistweaver <bryce.a.egley@gmail.com>
2020-08-12 04:21:55 +0000

    Arduino changes

--- arduino/light_controller/light_controller_8_8_2020/light_controller_8_8_2020.ino
+++ arduino/light_controller/light_controller_8_8_2020/light_controller_8_8_2020.ino
@@ -1,4 +1,4 @@
-/*
+         ls/*
     Aquarium Light Controller
     Board: Krida PWM1ChKrida PWM AC Light Dimmer Module 50Hz 60Hz For Arduino and Raspberry LED Smart Home
     Temperature Probe: DS18B20
