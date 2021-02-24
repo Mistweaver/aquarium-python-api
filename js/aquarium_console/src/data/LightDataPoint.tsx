5d90d663bc86dd8de6cfb9144a07bbd5233f1fe4
Mistweaver <bryce.a.egley@gmail.com>
2021-02-24 01:05:24 -0700

    Removed all the JS and microcontroller code.  Just old python code now.

--- js/aquarium_console/src/data/LightDataPoint.tsx
+++
@@ -1,9 +0,0 @@
-export class LightDataPoint {
-    time: number;
-    level: number;
-
-    constructor() {
-        this.time = 0;
-        this.level = 0;
-    }
-}
--- python/TestFunction.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def do_something():
-    count = 1
-    while True:
-        time.sleep(2)
-        print("Doing something ")
-        count += 1
